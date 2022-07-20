85269f2b54ea6221871c41cf178b85be39bfe4ac
litundebata <litundebata@gmail.com>
2022-07-20 21:19:45 +0530

    day-2 issues closed with all tasks

---
+++ scripts/homepage.js
@@ -0,0 +1,3 @@
+import {cardAboveFooter, footer} from "../components/combinedFooter.js";
+document.querySelector("#footer").innerHTML = footer();
+document.querySelector("#cardAboveFooter").innerHTML = cardAboveFooter();
