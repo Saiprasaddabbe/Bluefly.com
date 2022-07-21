788f514d62934a9d18838876dd676edf20d24e62
Vijay Yadav <vijyad12345@gmail.com>
2022-07-21 21:52:31 +0530

    day-3 progress

---
+++ ShoesPage/scripts/index.js
@@ -0,0 +1,47 @@
+
+
+import {cardAboveFooter,footer} from "/swank-talk-7472/components/combinedFooter.js"
+document.getElementById("cardAboveFooter").innerHTML = cardAboveFooter();
+document.getElementById("footer").innerHTML = footer();
+
+import { navbar } from "../../components/navbar.js";
+document.getElementById("navbar").innerHTML = navbar()
+
+
+
+
+   
+var container = document.getElementById("product")
+shoesData.forEach(function (elem) {
+   
+    var div = document.createElement("div")
+    var img = document.createElement("img")
+    var title = document.createElement("p")
+    var price = document.createElement("p")
+    var brand = document.createElement("p")
+    var label = document.createElement("p")
+    var disc = document.createElement("p")
+
+
+    img.setAttribute("src", elem.image)
+    title.innerText = elem.title;
+    price.innerText = elem.price;
+    brand.innerText = elem.brand;
+    label.innerText = elem.label;
+    disc.innerText = elem.discPrice;
+
+   div.addEventListener("click",function(){
+    addCart()
+   })
+   let inAline = document.createElement("div")
+   inAline.append(price,disc)
+
+    div.append(img,brand,title,inAline,label)
+
+    container.append(div)
+})
+ 
+
+function addCart(){
+    console.log("al;kdjfakjdskfa")
+}
