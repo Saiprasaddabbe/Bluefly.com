18e1e78c51052f68f7991ca98a57f7f89f8c69b7
Saiprasaddabbe <dabbesaiprasad8@gmail.com>
2022-07-22 01:32:33 +0530

    added navbar hover

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,5 @@
 let navbar = () => {
-  return ` <div id="first">
+  return `    <div id="first">
   <div class="logo">
     <img
       src="https://cdn.shopify.com/s/files/1/0248/3473/6191/files/BLUEFLY-LOGO-11-20.png?v=1574278243"
@@ -262,13 +262,221 @@ let navbar = () => {
     </span>
   </p>
   <p class="tooltip">
-    DESIGNERS<span class="tooltiptext">Tooltip text</span>
+    DESIGNERS<span class="tooltiptext">
+      <table>
+        <thead>
+          <th>FEATURED DESIGNERS</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Balenciaga</td>
+            <td>Bottega Veneta</td>
+          </tr>
+
+          <tr>
+            <td>Burberry</td>
+            <td>Celine</td>
+          </tr>
+
+          <tr>
+            <td>Chloe</td>
+            <td>Christian Louboutin</td>
+          </tr>
+
+          <tr>
+            <td>Dior</td>
+            <td>Dolce & Gabbana</td>
+          </tr>
+
+          <tr>
+            <td>Giuseppe Zanotti</td>
+            <td>Fendi</td>
+          </tr>
+
+          <tr>
+            <td>Givenchy</td>
+            <td>Gucci</td>
+          </tr>
+
+          <tr>
+            <td>Jimmy Choo</td>
+            <td>Kate Spade</td>
+          </tr>
+          <tr>
+            <td>Moncler</td>
+            <td>Moose Knucles</td>
+          </tr>
+
+          <tr>
+            <td>Moschino</td>
+            <td>Prada</td>
+          </tr>
+
+          <tr>
+            <td>Roberto Cavalli</td>
+            <td>Saint Laurent</td>
+          </tr>
+
+          <tr>
+            <td>Salvatore Ferragamo</td>
+            <td>Stella McCartney</td>
+          </tr>
+
+          <tr>
+            <td>Stuart Weitzman</td>
+            <td>Tod's</td>
+          </tr>
+
+          <tr>
+            <td>Tom Ford</td>
+            <td>Valentino</td>
+          </tr>
+
+          <tr>
+            <td>Versace</td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
   </p>
   <p class="tooltip">
-    SUNGLASSSES<span class="tooltiptext">Tooltip text</span>
+    SUNGLASSSES<span class="tooltiptext">
+      <table>
+        <thead>
+          <th>WOMEN'S SUNGLASSSES</th>
+          <th>MEN'S SUNGLASSSES</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shop All</td>
+
+            <td>Shop All</td>
+          </tr>
+          <tr>
+            <td>Boots</td>
+            <td>Chloe</td>
+          </tr>
+          <tr>
+            <td>Espadrilles</td>
+
+            <td>Drivers, Loafers & Slip-Ons</td>
+          </tr>
+          <tr>
+            <td>Oxford & Derby Shoes</td>
+            <td>Christian Louboutin</td>
+          </tr>
+          <tr>
+            <td>Giuseppe Zanotti</td>
+            <td>Sandals & Flip Flops</td>
+          </tr>
+          <tr>
+            <td>Gucci</td>
+            <td>Slippers</td>
+          </tr>
+          <tr>
+            <td>Burberry</td>
+            <td>Shop All</td>
+          </tr>
+          <tr>
+            <td>Chloe</td>
+            <td>Boots</td>
+          </tr>
+          <tr>
+            <td>Christian Louboutin</td>
+            <td>Drivers, Loafers & Slip-Ons</td>
+          </tr>
+          <tr>
+            <td>Flats</td>
+
+            <td>Christian Louboutin</td>
+          </tr>
+          <tr>
+            <td>Mules & Slides</td>
+
+            <td>Sandals & Flip Flops</td>
+          </tr>
+          <tr>
+            <td>Gucci</td>
+            <td>Slippers</td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
   </p>
   <p class="tooltip">
-    JEWELRY & WATCHES<span class="tooltiptext">Tooltip text</span>
+    JEWELRY & WATCHES<span class="tooltiptext">
+      <table>
+        <thead>
+          <th>JEWELRY</th>
+          <th>TOP DESIGNER WATCHES A-J</th>
+          <th>TOP DESIGNER WATCHES K-Z</th>
+          <th>ACCESSORIES</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Shop All</td>
+            <td>Backpacks</td>
+            <td>Bucket Bags</td>
+            <td>Clutches</td>
+          </tr>
+          <tr>
+            <td>Crossbody Bags</td>
+            <td>Hobo Bags</td>
+            <td>Satchels</td>
+            <td>Shoulder Bags</td>
+          </tr>
+          <tr>
+            <td>Tote Bag</td>
+            <td>Wallets & Cardholders</td>
+            <td>Balenciaga</td>
+            <td>Bottega Veneta</td>
+          </tr>
+          <tr>
+            <td>Burberry</td>
+            <td>Celine</td>
+            <td>Chloe</td>
+            <td>Dolce & Gabbana</td>
+          </tr>
+          <tr>
+            <td>Fendi</td>
+            <td>Givenchy</td>
+            <td>Gucci</td>
+            <td>Prada</td>
+          </tr>
+          <tr>
+            <td>Salvatore Ferragamo</td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Saint Laurent</td>
+            <td>Stella McCartney</td>
+            <td>Valentino</td>
+            <td>Shop All</td>
+          </tr>
+          <tr>
+            <td>Bucket Bags</td>
+            <td>Clutches</td>
+            <td>Crossbody Bags</td>
+            <td>Hobo Bags</td>
+          </tr>
+          <tr>
+            <td>Satchels</td>
+            <td>Shoulder Bags</td>
+            <td>Tote Bag</td>
+            <td>Wallets & Cardholders</td>
+          </tr>
+          <tr>
+            <td>Bag Charms, Straps</td>
+            <td>Belts</td>
+            <td>Cosmetics Bag</td>
+            <td>Hats</td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
   </p>
   <p class="tooltip">
     UNDER $ 50<span class="tooltiptext">
@@ -303,15 +511,102 @@ let navbar = () => {
     </span>
   </p>
   <p class="tooltip">
-    BEAUTY<span class="tooltiptext">Tooltip text</span>
+    BEAUTY<span class="tooltiptext">
+      <table>
+        <thead>
+          <th>FRAGRANCE</th>
+          <th>SKINCARE</th>
+          <th>HAIRCARE</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shop All</td>
+            <td>Shop All</td>
+            <td>Shop All</td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
   </p>
 </div>
 <div id="clearance" style="text-align: center">
   <p class="tooltip">
-    CLEARANCE<span class="tooltiptext">Tooltip text</span>
+    CLEARANCE<span class="tooltiptext">
+      <table>
+        <thead>
+          <th>MEN'S</th>
+          <th>WOMEN'S</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Activewear</td>
+            <td>Activewear</td>
+          </tr>
+          <tr>
+            <td>Coats & Jackets</td>
+            <td>Casual Button-Down Shirts</td>
+          </tr>
+          <tr>
+            <td>Dresses</td>
+            <td>Coats & Jackets</td>
+          </tr>
+          <tr>
+            <td>Jeans & Denim</td>
+            <td>Dress Shirts</td>
+          </tr>
+          <tr>
+            <td>Jumpsuits & Rompers</td>
+            <td>Jeans & Denim</td>
+          </tr>
+          <tr>
+            <td>Lingerie & Hosiery</td>
+            <td>Pants</td>
+          </tr>
+          <tr>
+            <td>Loungewear & Sleepwear</td>
+            <td>Polo Shirts</td>
+          </tr>
+          <tr>
+            <td>Pants & Leggings</td>
+            <td>Shorts</td>
+          </tr>
+
+          <tr>
+            <td>Shorts Skirts</td>
+            <td>Sport Coats & Blazers</td>
+          </tr>
+          <tr>
+            <td>Sweaters</td>
+            <td>Suits & Separates</td>
+          </tr>
+          <tr>
+            <td>Sweatshirts & Hoodies</td>
+            <td>Sweaters</td>
+          </tr>
+          <tr>
+            <td>Swimwear & Coverups</td>
+            <td>Sweatshirts & Hoodies</td>
+          </tr>
+
+          <tr>
+            <td>Tops & Tees</td>
+            <td>Swimwear</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>T-Shirts</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Underwear & Socks</td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
   </p>
 </div>`;
 };
 
-document.getElementById("navbar").innerHTML = navbar();
+// document.getElementById("navbar").innerHTML = navbar();
 export { navbar };
