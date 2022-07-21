0f1f37de8b3b29ed2b829a916e6e7bda0114e270
Gautam2s0 <sonkarg117@gmail.com>
2022-07-21 21:52:58 +0530

    day3

--- scripts/cart.js
+++ scripts/cart.js
@@ -32,8 +32,9 @@ let Data = [
     color: "37 / Orange",
   },
 ];
-
-let CartData = JSON.parse(localStorage.getItem("cart"));
+// <-------------display cart data------------------------------->
+localStorage.setItem("hello", JSON.stringify(Data));
+let CartData = JSON.parse(localStorage.getItem("cart")) || [];
 
 let display = (Data) => {
   let maindiv = document.querySelector("#gproduct");
@@ -44,16 +45,26 @@ let display = (Data) => {
     let div3 = document.createElement("div");
     let div4 = document.createElement("div");
     let div5 = document.createElement("div");
+    let div6 = document.createElement("div");
+    div6.innerHTML = `<div class="gp"> <i class="fa-solid fa-minus"></i></div><div id='N_item'>1</div>
+    <iv><i class="fa-solid fa-plus"></i></iv>`
+    div6.setAttribute("id", "gautam");
+    // div6.addEventListener("click",()=>{
+    //   console.log("hello")
+    // })
     let img = document.createElement("img");
     img.src = Data[i].image_url;
-    let name = document.createElement("h3");
+    let name = document.createElement("p");
     name.innerText = Data[i].name;
     let cost = document.createElement("p");
-    cost.innerText = Data[i].price;
+    cost.innerText = `$ ${Data[i].price}`;
     let color = document.createElement("p");
     color.innerText = Data[i].color;
+    let tot = document.createElement("p");
+    tot.innerText = `$ ${Data[i].price}`;
     let button = document.createElement("button");
     button.innerText = "REMOVE";
+    button.setAttribute("class", "rbtn");
     button.addEventListener("click", function () {
       let p = Data[i];
       let index = i;
@@ -62,18 +73,47 @@ let display = (Data) => {
     div1.append(img);
     div2.append(name, color, button);
     div3.append(div1, div2);
-    div4.append(cost);
+    div4.append(cost, div6, tot);
     div5.append(div3, div4);
 
     maindiv.append(div5);
   }
-  document.querySelector("#num>h3").innerText = count;
-  document.querySelector("#price>h3").innerText = "Total ₹: " + sum;
+
 };
+
+// ----------------------------delete item------------------------------------>
 display(Data);
-let data = JSON.parse(localStorage.getItem("cartdata")) || [];
+let data = JSON.parse(localStorage.getItem("hello")) || [];
 function deleter(index) {
   data.splice(index, 1);
-  localStorage.setItem("cartdata", JSON.stringify(data));
+  localStorage.setItem("hello", JSON.stringify(data));
   display(data);
 }
+
+// ---------------------------------------------increment decrement html---------------------------->
+function Inc_decr() {
+  return `
+  <div id="gp"> <i class="fa-solid fa-minus"></i></div>
+<div id='N_item'>1</div>
+<div><i class="fa-solid fa-plus"></i></div>`;
+}
+
+let dec = document.getElementsByClassName("gp")
+console
+// ----------------------------------increment decrement js-------------------------------------------->
+// let ff = document.querySelector('#gp').innerHTML;
+// ff.addEventListener("click",gautam)
+// function gautam(){
+//   console.log("hello")
+// }
+// console.log(ff)
+
+
+
+
+
+import { navbar } from "../components/navbar.js";
+document.querySelector("#navbar").innerHTML = navbar();
+import { cardAboveFooter, footer } from "../components/combinedFooter.js";
+document.querySelector("#footer").innerHTML = footer();
+document.querySelector("#cardAboveFooter").innerHTML = cardAboveFooter();
