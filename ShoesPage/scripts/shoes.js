96f60c4c9eccf5a42e152f9f62b127ea64002326
Vijay Yadav <vijyad12345@gmail.com>
2022-07-23 20:36:49 +0530

    fw18_0532 day-5 update

--- ShoesPage/scripts/shoes.js
+++ ShoesPage/scripts/shoes.js
@@ -1,44 +1,50 @@
-import {
-  cardAboveFooter,
-  footer,
-} from "/swank-talk-7472/components/combinedFooter.js";
+
+
+import {cardAboveFooter,footer} from "../../components/combinedFooter.js"
 document.getElementById("cardAboveFooter").innerHTML = cardAboveFooter();
 document.getElementById("footer").innerHTML = footer();
 
 import { navbar } from "../../components/navbar.js";
-// document.getElementById("navbar").innerHTML = navbar() //commented by saiprasad
+document.getElementById("navbar").innerHTML = navbar()
 
 import { sidebar } from "../components/sidebar.js";
-document.getElementById("sidebar").innerHTML = sidebar();
+document.getElementById("sidebar").innerHTML = sidebar()
 
-var container = document.getElementById("product");
-shoesData.forEach(function (elem) {
-  var div = document.createElement("div");
-  var img = document.createElement("img");
-  var title = document.createElement("p");
-  var price = document.createElement("p");
-  var brand = document.createElement("p");
-  var label = document.createElement("p");
-  var disc = document.createElement("p");
 
-  img.setAttribute("src", elem.image);
-  title.innerText = elem.title;
-  price.innerText = elem.price;
-  brand.innerText = elem.brand;
-  label.innerText = elem.label;
-  disc.innerText = elem.discPrice;
 
-  div.addEventListener("click", function () {
-    addCart();
-  });
-  let inAline = document.createElement("div");
-  inAline.append(price, disc);
+   
+let container = document.getElementById("product")
+shoesData.forEach((elem,index)=> {
+   
+    let div = document.createElement("div")
+    let img = document.createElement("img")
+    let title = document.createElement("p")
+    let price = document.createElement("p")
+    let brand = document.createElement("p")
+    let label = document.createElement("p")
+    let disc = document.createElement("p")
 
-  div.append(img, brand, title, inAline, label);
 
-  container.append(div);
-});
+    img.src = elem.image;
+    title.innerText = elem.title;
+    price.innerText = elem.price;
+    brand.innerText = elem.brand;
+    brand.style.textAlign= "center";
+    brand.style.marginLeft = "8%"
+    label.innerText = elem.label;
+    disc.innerText = elem.discPrice;
 
-function addCart() {
-  console.log("al;kdjfakjdskfa");
-}
+   div.addEventListener("click",()=>{
+    localStorage.setItem("detail",JSON.stringify(elem))
+    window.location.href = "display.html"
+    // addCart(elem)
+   })
+   let inAline = document.createElement("div")
+   inAline.append(price,disc)
+
+    div.append(img,brand,title,inAline,label)
+
+    container.append(div)
+})
+ 
+
